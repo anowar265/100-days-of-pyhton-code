30e4644048f40d43c3a7cde25946b9b5515fc206
Anowar Hossain <91547105+anowar265@users.noreply.github.com>
2024-06-10 16:44:58 +0000

    updated day1

--- 01-Day1-Intro-to-Python/main.py
+++ 01-Day1-Intro-to-Python/main.py
@@ -1,3 +1,16 @@
 print("Hello World!")
 print(2)
 print("start")
+
+#Angelena Yu Lecture
+
+print("Day1 - Python print function")
+print("The function is declared like this:")
+print("print(\"what to print\")")
+
+print("Welcome to the new Band-Name-Genereator")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
+
+#Manipulation and Code Inteligence
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-import pandas
-print("Welcome to Module and pip")
+#import pandas
+#print("Welcome to Module and pip")
+
+#Angela Lecture
